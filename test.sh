84aaab2cdf5192bab8f4fd995d701c1372377c4b
Ganna Starovoytova <gstarovo@redhat.com>
2025-09-03 12:30:15 +0200

    run no tests, just setup

--- test.sh
+++ test.sh
@@ -69,54 +69,54 @@ rlJournalStart
         rlRun "touch ${SERVER_TXT}"
     rlPhaseEnd
 
-    rlPhaseStartTest "TEST 1: Default connection with X25519MLKEM768"
-        start_s_server $KEY $CRT 4433
-        run_s_client_and_grep "" "localhost" "4433" "NamedGroup: X25519MLKEM768" "" ""
-        stop_s_server
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 1: Default connection with X25519MLKEM768"
+    #     start_s_server $KEY $CRT 4433
+    #     run_s_client_and_grep "" "localhost" "4433" "NamedGroup: X25519MLKEM768" "" ""
+    #     stop_s_server
+    # rlPhaseEnd
 
-    rlPhaseStartTest "TEST 2: Specifying groups: SecP256r1MLKEM768 and X25519MLKEM768"
-        start_s_server $KEY $CRT 4433
-        rlLogInfo "Specify the group SecP256r1MLKEM768"
-        run_s_client_and_grep "-groups SecP256r1MLKEM768" "localhost" "4433" "Shared groups: SecP256r1MLKEM768" "" ""
-        rlLogInfo "Specify the group X25519MLKEM768"
-        run_s_client_and_grep "-groups X25519MLKEM768" "localhost" "4433" "Shared groups: X25519MLKEM768" "" ""
-        stop_s_server
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 2: Specifying groups: SecP256r1MLKEM768 and X25519MLKEM768"
+    #     start_s_server $KEY $CRT 4433
+    #     rlLogInfo "Specify the group SecP256r1MLKEM768"
+    #     run_s_client_and_grep "-groups SecP256r1MLKEM768" "localhost" "4433" "Shared groups: SecP256r1MLKEM768" "" ""
+    #     rlLogInfo "Specify the group X25519MLKEM768"
+    #     run_s_client_and_grep "-groups X25519MLKEM768" "localhost" "4433" "Shared groups: X25519MLKEM768" "" ""
+    #     stop_s_server
+    # rlPhaseEnd
 
-    rlPhaseStartTest "TEST 3: Hybrid ML-KEM - TLS connection with oqs test server"
-        rlLogInfo "Connection with SecP256r1MLKEM768"
-        run_s_client_and_grep "" "test.openquantumsafe.org" "6001" 'CONNECTED\(00000003\);(Successfully connected using )([a-z]|[A-Z]|[0-9])*(-)*SecP256r1MLKEM768' "-P" "using"
-        rlLogInfo "Connection with X25519MLKEM768"
-        run_s_client_and_grep "" "test.openquantumsafe.org" "6003" 'CONNECTED\(00000003\);(Successfully connected using )([a-z]|[A-Z]|[0-9])*(-)*X25519MLKEM768' "-P" "using"
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 3: Hybrid ML-KEM - TLS connection with oqs test server"
+    #     rlLogInfo "Connection with SecP256r1MLKEM768"
+    #     run_s_client_and_grep "" "test.openquantumsafe.org" "6001" 'CONNECTED\(00000003\);(Successfully connected using )([a-z]|[A-Z]|[0-9])*(-)*SecP256r1MLKEM768' "-P" "using"
+    #     rlLogInfo "Connection with X25519MLKEM768"
+    #     run_s_client_and_grep "" "test.openquantumsafe.org" "6003" 'CONNECTED\(00000003\);(Successfully connected using )([a-z]|[A-Z]|[0-9])*(-)*X25519MLKEM768' "-P" "using"
+    # rlPhaseEnd
 
-    rlPhaseStartTest "TEST 4: Tests with the nginx server"
-        rlRun "nginx"
-        run_s_client_and_grep "" "localhost" "443" "CONNECTED(00000003);NamedGroup: X25519MLKEM768" "" ""
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 4: Tests with the nginx server"
+    #     rlRun "nginx"
+    #     run_s_client_and_grep "" "localhost" "443" "CONNECTED(00000003);NamedGroup: X25519MLKEM768" "" ""
+    # rlPhaseEnd
 
-    rlPhaseStartTest "TEST 5: Tests with curl"
-        rlRun "curl --cacert $CRT https://localhost:443/ -o /dev/null" 0 "Curl command exit status"
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 5: Tests with curl"
+    #     rlRun "curl --cacert $CRT https://localhost:443/ -o /dev/null" 0 "Curl command exit status"
+    # rlPhaseEnd
 
-    rlPhaseStartTest "TEST 6: List the supported ML-KEM algorithms"
-        rlRun -s "openssl list -kem-algorithms -provider oqsprovider"
-        rlAssertGrep "SecP256r1MLKEM768" $rlRun_LOG
-        rlAssertGrep "X25519MLKEM768" $rlRun_LOG
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 6: List the supported ML-KEM algorithms"
+    #     rlRun -s "openssl list -kem-algorithms -provider oqsprovider"
+    #     rlAssertGrep "SecP256r1MLKEM768" $rlRun_LOG
+    #     rlAssertGrep "X25519MLKEM768" $rlRun_LOG
+    # rlPhaseEnd
 
-    rlPhaseStartTest "TEST 7: Testing a ML-DSA Key"
-        rlLogInfo "Generating a ML-DSA Key Pair"
-        rlRun "openssl genpkey -algorithm mldsa65 -out mldsa65_private.pem"
-        rlRun "openssl pkey -in mldsa65_private.pem -pubout -out mldsa65_public.pem"
-        rlLogInfo "Signing a raw message"
-        rlRun "seq 1 10 > message.txt"
-        rlRun "openssl dgst -sign mldsa65_private.pem -out signature.bin message.txt"
-        rlLogInfo "Verifying the signature"
-        rlRun -s "openssl dgst -verify mldsa65_public.pem -signature signature.bin message.txt"
-        rlAssertGrep "Verified OK" $rlRun_LOG
-    rlPhaseEnd
+    # rlPhaseStartTest "TEST 7: Testing a ML-DSA Key"
+    #     rlLogInfo "Generating a ML-DSA Key Pair"
+    #     rlRun "openssl genpkey -algorithm mldsa65 -out mldsa65_private.pem"
+    #     rlRun "openssl pkey -in mldsa65_private.pem -pubout -out mldsa65_public.pem"
+    #     rlLogInfo "Signing a raw message"
+    #     rlRun "seq 1 10 > message.txt"
+    #     rlRun "openssl dgst -sign mldsa65_private.pem -out signature.bin message.txt"
+    #     rlLogInfo "Verifying the signature"
+    #     rlRun -s "openssl dgst -verify mldsa65_public.pem -signature signature.bin message.txt"
+    #     rlAssertGrep "Verified OK" $rlRun_LOG
+    # rlPhaseEnd
 
 rlJournalPrintText
 rlJournalEnd
