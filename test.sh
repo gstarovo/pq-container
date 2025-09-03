51a79959625e57557739b16ddd89d664973fc150
Ganna Starovoytova <gstarovo@redhat.com>
2025-09-03 13:46:30 +0200

    #1 test modify: only start openssl server, save the pid, add echo, exclude waiting, proceed in test

--- test.sh
+++ test.sh
@@ -71,8 +71,8 @@ rlJournalStart
 
     rlPhaseStartTest "TEST 1: Default connection with X25519MLKEM768"
         start_s_server $KEY $CRT 4433
-        # run_s_client_and_grep "" "localhost" "4433" "NamedGroup: X25519MLKEM768" "" ""
-        # stop_s_server
+        run_s_client_and_grep "" "localhost" "4433" "NamedGroup: X25519MLKEM768" "" ""
+        stop_s_server
     rlPhaseEnd
 
     # rlPhaseStartTest "TEST 2: Specifying groups: SecP256r1MLKEM768 and X25519MLKEM768"
